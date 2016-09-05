cce2d500ff7da5c1a09a89601289c78a64146240
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2016-09-05 16:24:49 +0200

    wip

---
+++ _sync.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Copies the selected configs over from my environment to this directory.
+This will only make sense to be run from inside our chair.
+"""
+
+import os, sys, re
+mydir = os.path.dirname(__file__) or "."
+
+quaero_experiments = [
+
+]
