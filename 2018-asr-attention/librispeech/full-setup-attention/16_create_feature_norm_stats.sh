af8852c0b73876a0183aac5a41466f0f79e94269
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2018-05-16 20:02:46 +0200

    feature norm script better

--- 2018-asr-attention/librispeech/full-setup-attention/16_create_feature_norm_stats.sh
+++ 2018-asr-attention/librispeech/full-setup-attention/16_create_feature_norm_stats.sh
@@ -7,8 +7,12 @@ cd data/dataset
 
 test -s stats.mean.txt && test -s stats.std_dev.txt && echo "stats.*.txt files already exist, exit" && exit
 
+# 'seq_ordering':'random' just to have a better estimate of remaining time.
+# Takes around 10h for me.
+# bpe stuff not really needed here, just to make it load.
+
 $mydir/returnn/tools/dump-dataset.py \
-  "{'class':'LibriSpeechCorpus', 'bpe':{'bpe_file':'trans.bpe.codes', 'vocab_file':'trans.bpe.vocab'}, 'path':'.', 'audio':{}, 'prefix': 'train', 'use_zip': True}" \
+  "{'class':'LibriSpeechCorpus', 'bpe':{'bpe_file':'trans.bpe.codes', 'vocab_file':'trans.bpe.vocab'}, 'path':'.', 'audio':{}, 'prefix': 'train', 'use_zip': True, 'seq_ordering':'random'}" \
   --endseq -1 --type null --dump_stats stats
 
 test -s stats.mean.txt && test -s stats.std_dev.txt
