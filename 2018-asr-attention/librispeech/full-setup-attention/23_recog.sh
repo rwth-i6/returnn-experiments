305dadc951b18c5c9f2ba935540346ea541b5e5f
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2018-05-17 16:03:47 +0200

    recog, some comments

--- 2018-asr-attention/librispeech/full-setup-attention/23_recog.sh
+++ 2018-asr-attention/librispeech/full-setup-attention/23_recog.sh
@@ -2,12 +2,19 @@
 
 set -exv
 
+# You can run this already while it is still training.
+# Run it a final time when the training is finished.
+
 experiment=returnn
 test -e data/exp-$experiment  # experiment existing?
 test -e data/exp-$experiment/train-scores.data  # some epochs trained?
 
+# Get us some recommended epochs to do recog on.
 epochs=$(./tools/recommend-recog-epochs.py --experiment $experiment)
 extra_args="-- ++batch_size 2000"  # such that it fits on your GPU
+
+# In recog, sequences are sorted by length, to optimize the batch search padding.
+# We start with the longest ones, to make sure the memory is enough.
 
 for epoch in $epochs; do
   echo "recog of epoch $epoch"
