a01333e72641fc32cd3c3ee9a7b5c443cc5c6da6
Nick Rossenbach <nick@sounddesign.com>
2020-05-03 22:44:41 +0200

    added comments to RETURNN launcher

--- 2019-asr-synthetic-data/sisyphus_project/bin/returnn_launcher.sh
+++ 2019-asr-synthetic-data/sisyphus_project/bin/returnn_launcher.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-echo "activating CUDA 10"
-source /work/iwslt/smt/rossenbach/returnn_env_cuda10/bin/activate
+echo "activating CUDA"
+# If you are not executing Sisyphus with a cuda env, you can add it here
+# source /path/to/your/cuda/env/bin/activate
 export PS1="" 
 export LD_LIBRARY_PATH="/usr/local/cuda-10.0/lib64:/usr/local/cuda-10.0/extras/CUPTI/lib64:/usr/local/cudnn-10.0-v7.4/lib64:/usr/lib/nvidia-418" 
 export CUDA_HOME=/usr/local/cuda-10.0/ 
-export CUDNN=/usr/local/cudnn-10.0-v7.4/ 
+export CUDNN=/usr/local/cudnn-10.0-v7.4/
+
+# Uncomment and set ID to limit execution to a specific GPU
+# export CUDA_VISIBLE_DEVICES=0
 
 echo $PYTHONPATH
 echo "$@"
