e62eaee40ea5ef1ea32b851baa901806f04324f0
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2020-08-20 16:32:13 +0200

    some more

--- 2020-librispeech-data-prepare/19_test_iter_corpus_via_config.sh
+++ 2020-librispeech-data-prepare/19_test_iter_corpus_via_config.sh
@@ -9,4 +9,7 @@ set -exv
 # For interactive terminals, you should see some progress bar like:
 # 353/822, 0:00:04 [|||....]
 
-./returnn/tools/dump-dataset.py returnn.config --endseq -1 --type null
+# You might want to pass `--epoch 11` or so, to see the epoch-wise filter in effect.
+# See `-h` for available options.
+
+./returnn/tools/dump-dataset.py returnn.config --endseq -1 $*
