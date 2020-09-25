654dbeea227a1a0069585c88ba65589a2c962807
Parnia Bahar <bahar@i6.informatik.rwth-aachen.de>
2020-09-25 21:59:14 +0200

    cleana up a little

--- 2020-st-tight-integration/extra_module/add.py
+++ 2020-st-tight-integration/extra_module/add.py
@@ -23,22 +23,3 @@ class AddOneHotToTime(CopyLayer):
     self.output.placeholder = tf.concat([self.output.placeholder, x * c], axis=self.output.time_dim_axis)
     self.output.size_placeholder[self.output.time_dim_axis_excluding_batch] += repeat
 
-class AddZero(CopyLayer):
-  layer_class = "addzero"
-
-  def __init__(self, repeat=1, **kwargs):
-    """
-    :param float|str prefix: either some constant or another layer
-    :param int repeat: how often to repeat the prefix
-    """
-    super(AddZero, self).__init__(**kwargs)
-    assert self.output.time_dim_axis is not None
-    c = tf.zeros([1.0], dtype=self.output.dtype)
-    shape = [((self.output.batch_shape[i] or tf.shape(self.output.placeholder)[i])
-              if (i != self.output.time_dim_axis)
-              else repeat)
-             for i in range(self.output.batch_ndim)]
-    x = tf.ones(shape, dtype=self.output.dtype)
-    self.output.placeholder = tf.concat([self.output.placeholder, x * c], axis=self.output.time_dim_axis)
-    self.output.size_placeholder[self.output.time_dim_axis_excluding_batch] += repeat
-
