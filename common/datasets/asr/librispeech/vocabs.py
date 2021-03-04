20212a04720447332c6d0066cec2dbed83b502be
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2021-03-04 15:02:19 +0100

    librispeech vocabs

--- common/datasets/asr/librispeech/vocabs.py
+++ common/datasets/asr/librispeech/vocabs.py
@@ -2,16 +2,20 @@
 This contains some of our standard vocabularies (e.g. BPE) for Librispeech.
 """
 
+from pathlib import Path
 from typing import Dict
 from ...interface import VocabConfig
 
 
 class _Bpe(VocabConfig):
-  def __init__(self, dim, codes, vocab):
+  def __init__(self, dim, codes: Path, vocab: Path):
     super(_Bpe, self).__init__()
     self.dim = dim
-    self.codes = codes
-    self.vocab = vocab
+    self.codes = str(codes)
+    self.vocab = str(vocab)
+
+  def get_num_classes(self) -> int:
+    return self.dim
 
   def get_opts(self) -> Dict[str]:
     return {
@@ -21,13 +25,16 @@ class _Bpe(VocabConfig):
     }
 
 
+_vocab_dir = Path(__file__).absolute().parent / "vocabs"
+
+
 bpe1k = _Bpe(
   dim=1056,
-  codes='data/dataset/trans.bpe1k.codes',
-  vocab='data/dataset/trans.bpe1k.vocab')
+  codes=_vocab_dir / 'trans.bpe1k.codes',
+  vocab=_vocab_dir / 'trans.bpe1k.vocab')
 
 
 bpe10k = _Bpe(
   dim=10025,
-  codes='data/dataset/trans.bpe10k.codes',
-  vocab='data/dataset/trans.bpe10k.vocab')
+  codes=_vocab_dir / 'trans.bpe10k.codes',
+  vocab=_vocab_dir / 'trans.bpe10k.vocab')
