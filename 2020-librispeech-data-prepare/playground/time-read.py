0e2d029da95557b498abc1821a73e66557ddc230
Albert Zeyer <albzey@gmail.com>
2021-03-04 17:44:26 +0100

    timing more

--- 2020-librispeech-data-prepare/playground/time-read.py
+++ 2020-librispeech-data-prepare/playground/time-read.py
@@ -13,7 +13,9 @@ txt_fn_gz = os.path.expanduser("train-other-500.txt.gz")
 t = time.time()
 txt = gzip.open(txt_fn_gz, "rb").read().decode("utf8")
 print("Gunzip + read time:", time.time() - t)
+print("Size:", len(txt))
 
-print(timeit.timeit(lambda: eval(txt), number=1))
-
-print(timeit.timeit(lambda: ast.literal_eval(txt), number=1))
+print("compile:", timeit.timeit(lambda: compile(txt, "<>", "exec"), number=1))
+print("parse:", timeit.timeit(lambda: compile(txt, "<>", "exec", ast.PyCF_ONLY_AST), number=1))
+print("eval:", timeit.timeit(lambda: eval(txt), number=1))
+print("ast.literal_eval:", timeit.timeit(lambda: ast.literal_eval(txt), number=1))
