4804dc46a5bc5c2d2d68941bade68ebecafc457e
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2021-03-24 12:29:42 +0100

    librispeech dataset also old defaults

--- common/datasets/asr/librispeech/oggzip.py
+++ common/datasets/asr/librispeech/oggzip.py
@@ -1,24 +1,40 @@
 
 from __future__ import annotations
+from pathlib import Path
 from typing import Dict, Any
 from ..features import make_gt_features_opts
-from .vocabs import bpe1k
+from .vocabs import bpe1k, bpe10k
 from ...interface import DatasetConfig, VocabConfig
 from ....data import get_common_data_path
+
 
 _Parts = [
   "train-clean-100", "train-clean-360", "train-other-500",
   "dev-clean", "dev-other",
   "test-clean", "test-other"]
 
+_norm_stats_dir = Path(__file__).absolute().parent / "norm_stats"
+
 
 class Librispeech(DatasetConfig):
-  def __init__(self, audio_dim=50, train_epoch_split=20, train_random_permute=None, vocab: VocabConfig = bpe1k):
+  def __init__(self, *,
+               audio_dim=50,
+               audio_norm: str = "per_seq",
+               vocab: VocabConfig = bpe1k,
+               train_epoch_split=20, train_random_permute=None):
+    """
+    :param audio_norm: "global" or "per_seq". "global" tries to read from standard location in repo
+    """
     super(Librispeech, self).__init__()
     self.audio_dim = audio_dim
+    self.audio_norm = audio_norm
+    self.vocab = vocab
     self.train_epoch_split = train_epoch_split
     self.train_random_permute = train_random_permute
-    self.vocab = vocab
+
+  @classmethod
+  def old_defaults(cls, audio_dim=40, audio_norm="global", vocab: VocabConfig = bpe10k, **kwargs) -> Librispeech:
+    return Librispeech(audio_dim=audio_dim, audio_norm=audio_norm, vocab=vocab, **kwargs)
 
   def get_extern_data(self) -> Dict[str, Dict[str, Any]]:
     return {
@@ -45,12 +61,25 @@ class Librispeech(DatasetConfig):
       files += [
         get_common_data_path("librispeech/dataset/dataset-ogg/%s.zip" % part),
         get_common_data_path("librispeech/dataset/dataset-ogg/%s.txt.gz" % part)]
+
+    def _make_norm_arg(k: str):
+      if self.audio_norm == "per_seq":
+        return "per_seq"
+      if self.audio_norm == "global":
+        return str(_norm_stats_dir / f"stats.{self.audio_dim}.{k}.txt")
+      if not self.audio_norm:
+        return None
+      raise TypeError(f"Invalid audio norm {self.audio_norm}.")
+
     d = {
       "class": 'OggZipDataset',
       "path": files,
       "zip_audio_files_have_name_as_prefix": False,
       "targets": self.vocab.get_opts(),
-      "audio": {"norm_mean": "per_seq", "norm_std_dev": "per_seq", "num_feature_filters": self.audio_dim},
+      "audio": {
+        "norm_mean": _make_norm_arg("mean"),
+        "norm_std_dev": _make_norm_arg("std_dev"),
+        "num_feature_filters": self.audio_dim},
       # make_gt_features_opts(dim=self.audio_dim),
     }  # type: Dict[str, Any]
     if train:
