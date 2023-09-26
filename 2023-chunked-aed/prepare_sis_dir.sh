b4ea570cf072316675a85974d8ca8084cc4d31a4
Mohammad Zeineldeen <mohammadzed5@gmail.com>
2023-09-26 19:50:50 +0200

    add prepare sis setup script

---
+++ 2023-chunked-aed/prepare_sis_dir.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -e
+
+dirname=$1
+
+if [ ! $# -eq 1 ]; then
+  echo "Usage: $0 <setup-dirname>"
+  exit -1
+fi
+
+mkdir -p $dirname
+cd $dirname
+
+echo "Cloning repos"
+
+git clone git@github.com:rwth-i6/sisyphus.git
+ln -s sisyphus/sis .
+
+git clone git@github.com:rwth-i6/returnn.git
+
+mkdir "recipe"
+git clone git@github.com:rwth-i6/i6_core.git recipe/i6_core
+git clone git@github.com:rwth-i6/i6_experiments.git recipe/i6_experiments
+
+echo "Creating setup dirs"
+
+mkdir "alias"  # sis jobs aliases (symlinks)
+mkdir "output"  # sis jobs outputs (symlinks)
+mkdir "work"  # sis jobs
+mkdir "config"  # main entry config is added here
+
+# sisyphus settings which needs to be adapted depending on the working environment
+cp recipe/i6_experiments/users/zeineldeen/experiments/chunkwise_att_2023/settings.py . 
+
+# create dir for pycharm project to avoid indexing huge dirs (e.g work)
+echo "Creating dir for PyCharm project"
+
+pycharmProj="`basename $PWD`-proj"
+mkdir $pycharmProj
+cd $pycharmProj
+ln -s ../returnn .
+ln -s ../sisyphus .
+ln -s ../settings.py .
+ln -s ../recipe .
+ln -s ../config .
