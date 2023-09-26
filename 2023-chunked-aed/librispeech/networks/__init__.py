88f52ad392b0d1b4eb80b39231907b43826398e6
Mohammad Zeineldeen <mohammadzed5@gmail.com>
2023-09-26 19:00:52 +0200

    add libri pretrain nets

---
+++ 2023-chunked-aed/librispeech/networks/__init__.py
@@ -0,0 +1,20 @@
+from .network_1 import network as network_1
+from .network_6 import network as network_6
+from .network_11 import network as network_11
+from .network_16 import network as network_16
+from .network_21 import network as network_21
+from .network_26 import network as network_26
+from .network_31 import network as network_31
+from .network_36 import network as network_36
+
+
+networks_dict = {
+    1: network_1,
+    6: network_6,
+    11: network_11,
+    16: network_16,
+    21: network_21,
+    26: network_26,
+    31: network_31,
+    36: network_36,
+}
